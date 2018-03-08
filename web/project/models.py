10d83504177339882573a706d05b97c6acaab51b
Jackson Kontny <jackson.kontny@gmail.com>
2018-03-07 19:51:52 -0600

    Add user model

--- web/project/models.py
+++ web/project/models.py
@@ -6,3 +6,11 @@ from flask import url_for
 import bleach
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
